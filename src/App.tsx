67cd0ca46c31f47c1bd4ce09e3b56c41f240204c
chinmaykd21 <chinmaykd21@gmail.com>
2025-01-29 15:44:44 -0500

    rollback: remove healthcheck for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { KeepBackEndAlive } from "./components/KeepBackEndAlive";
 import { TextEditor } from "./components/TextEditor";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <KeepBackEndAlive />
       <TextEditor />
     </div>
   );
